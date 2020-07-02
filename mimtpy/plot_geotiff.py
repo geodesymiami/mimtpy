68896e58b95bfa0ef5f514780053b7decbe9eda4
ranneylxr <lvxr@radi.ac.cn>
2020-07-02 21:56:28 +0800

    plot_geotiff.py:add plot NGLStation gps velocity vector field

--- mimtpy/plot_geotiff.py
+++ mimtpy/plot_geotiff.py
@@ -15,15 +15,37 @@ import matplotlib.pyplot as plt
 import geopandas
 import numpy as np
 
+from mintpy.objects import timeseries
+from datetime import datetime as dt
+import mintpy.objects.gps as gps
+from mintpy.objects.gps import GPS
+from mintpy.utils import utils as ut
+
+
 min_figsize_single = 6.0       # default min size in inch, for single plot
 max_figsize_single = 10.0      # default min size in inch, for single plot
 max_figsize_height = 8.0       # max figure size in vertical direction in inch
 
 ######################################################################################
 EXAMPLE = """example:
+   
+    # only plot faults and reference points
     plot_geotiff.py geotiff_file --shpdir /data/lxrtest/Balochistan/shp/ --fault multifault.shp nearbyfault.shp --fcolor b r --fstyle s d --refpoi refpoi_DT.shp --outfile velocity --outdir /data/lxrtest/BalochistanSenAT/shp/ 
     
     plot_geotiff.py geotiff_file --shpdir /data/lxrtest/Balochistan/shp/ --fault multifault.shp nearbyfault.shp --fcolor o m --fstyle s s --refpoi refpoi_DT.shp --vlim -0.02 0.02 --outfile velocity --outdir /data/lxrtest/BalochistanSenAT/shp/ 
+   
+    # plot shape file and gps velocity vector field
+    # plot single gps site with extracting incidence and heading angle from geometreRadar 
+    plot_geotiff.py geotiff_file --shpdir /data/lxrtest/Balochistan/shp/ --fault multifault.shp nearbyfault.shp --fcolor o m --fstyle s s --refpoi refpoi_DT.shp --gps_putton --gps_name GV05 --geometry $SCRATCHDIR/BalochistanSenAT115/mintpy/inputs/geometryRadar.h5 --gpsdir /data/lxr/insarlab/GPS/ --vlim -0.02 0.02 --outfile velocity --outdir /data/lxrtest/BalochistanSenAT/shp/ 
+    
+    # plot single gps site using typical incidence and heading angle for Seitinel1
+    plot_geotiff.py geotiff_file --shpdir /data/lxrtest/Balochistan/shp/ --fault multifault.shp nearbyfault.shp --fcolor o m --fstyle s s --refpoi refpoi_DT.shp --gps_putton --gps_name GV05 --gpsdir /data/lxr/insarlab/GPS/ --vlim -0.02 0.02 --outfile velocity --outdir /data/lxrtest/BalochistanSenAT/shp/ 
+    
+    # plot multiple gps sites with extracting incidence and heading angle from geometreRadar 
+    plot_geotiff.py geotiff_file --shpdir /data/lxrtest/Balochistan/shp/ --fault multifault.shp nearbyfault.shp --fcolor o m --fstyle s s --refpoi refpoi_DT.shp --gps_putton --search --bbox 36 40 36 39 --geometry $SCRATCHDIR/BalochistanSenAT115/mintpy/inputs/geometryRadar.h5 --gpsdir /data/lxr/insarlab/GPS/ --vlim -0.02 0.02 --outfile velocity --outdir /data/lxrtest/BalochistanSenAT/shp/ 
+    
+    # plot multiple gps sites with extracting incidence and heading angle from geometreRadar 
+    plot_geotiff.py geotiff_file --shpdir /data/lxrtest/Balochistan/shp/ --fault multifault.shp nearbyfault.shp --fcolor o m --fstyle s s --refpoi refpoi_DT.shp --gps_putton --search --bbox 36 40 36 39 --gpsdir /data/lxr/insarlab/GPS/ --vlim -0.02 0.02 --outfile velocity --outdir /data/lxrtest/BalochistanSenAT/shp/ 
 """
 
 fcolor_table = {'b':'black','r':'red','y':'yellow','o':'orange','m':'megenta'}
@@ -36,23 +58,41 @@ def create_parser():
 
     parser.add_argument('input_geotiff', nargs=1, type=str, help='geotiff file. \n')
 
-    parser.add_argument('--shpdir', nargs=1, type=str, help='directory of shp files. \n')
+    shp_option = parser.add_argument_group(title='option for shape files (faults and reference point).')   
+ 
+    shp_option.add_argument('--shpdir', dest='shpdir', nargs=1, type=str, help='directory of shp files. \n')
 
-    parser.add_argument('--fault', nargs='+', type=str, help='shp files of fault. \n')
+    shp_option.add_argument('--fault', dest='fault', nargs='+', type=str, help='shp files of fault. \n')
     
-    parser.add_argument('--fcolor', nargs='+', type=str, help='define color for faults:'
+    shp_option.add_argument('--fcolor', dest='fcolor', nargs='+', type=str, help='define color for faults:'
                                                               'b -> black; r -> red; y -> yellow;'
                                                               'o -> orange; m-> magenta \n')
-    parser.add_argument('--fstyle', nargs='+', type=str, help='define line stype for faults:'
+    shp_option.add_argument('--fstyle', dest='fstyle', nargs='+', type=str, help='define line stype for faults:'
                                                               's -> solid; d -> dashed \n')
-    parser.add_argument('--refpoi', nargs=1, type=str, help='shp file of reference point. \n')
+    shp_option.add_argument('--refpoi', dest='refpoi', nargs=1, type=str, help='shp file of reference point. \n')
    
-    parser.add_argument('--vlim', dest = 'vlim', nargs=2, type=float, help='max and min value of colorbar.the unit is m. \n')   
+    gps_option = parser.add_argument_group(title='options for plot gps velocity vector field')
+    
+    gps_option.add_argument('--gps_putton', action='store_true', default=False, help='whether plot gps velocity vector field. \n')
+
+    gps_option.add_argument('--search', action='store_true', default=False, help='whether search gps sites based on the given SNWE. \n')
+   
+    gps_option.add_argument('--bbox', dest='SNWE', type=float, nargs=4, metavar=('S', 'N','W','E'),
+                            help='Bounding box of area to be geocoded.\n'+
+                            'Include the uppler left corner of the first pixel' + 
+                            'and the lower right corner of the last pixel')
+    gps_option.add_argument('--gps_name', dest='gps_name', nargs='?', type=str, help='gps site name. \n')
+
+    gps_option.add_argument('--geometry', dest='geometry', nargs='?', type=str, help='geometry data. \n')
+
+    gps_option.add_argument('--gpsdir', dest='gpsdir',nargs=1, type=str, help='directory to store gps data. \n')
+
+    parser.add_argument('--vlim', dest = 'vlim', nargs=2, type=float, help='max and min value for displacement to display. The unit is m. \n')   
  
     parser.add_argument('--outdir',dest='outdir',nargs=1, type = str, help='output directory.\n')
 
     parser.add_argument('--outfile',dest='outfile', nargs=1, type=str, help='output file name.\n')
-    
+
     return parser
 
 def cmd_line_parse(iargs=None):
@@ -91,7 +131,63 @@ def flinestyle(fstyles, shp_faults):
 
     return fault_linestyles
 
-def plot_geotiff(inps):
+def gps_multisites(gpsname, geometry, gps_dir):
+    """obtain multiple gps sites velocity field"""
+    gps_obj = GPS(site=gpsname, data_dir=gps_dir)
+    gps_obj.open()
+    if not geometry:
+        dis_los = ut.enu2los(gps_obj.dis_e, gps_obj.dis_n, gps_obj.dis_u)
+        dates = gps_obj.dates
+        los_vel = dis_velocity(dates,dis_los) 
+    else:
+        dates, dis_los = gps_obj.read_gps_los_displacement(geometry)
+        gps_obj.get_gps_los_velocity(geometry)
+        los_vel = gps_obj.velocity
+    
+    e_vel = dis_velocity(dates, gps_obj.dis_e)
+    n_vel = dis_velocity(dates, gps_obj.dis_n)
+   
+    return np.array([[los_vel, n_vel, e_vel]])
+
+def gps_single(gpsname, geometry, gps_dir):
+    """process single gps site""" 
+    gps_obj = GPS(site=gpsname, data_dir=gps_dir)
+    gps_obj.open()
+    if not geometry:
+        dis_los = ut.enu2los(gps_obj.dis_e, gps_obj.dis_n, gps_obj.dis_u)
+        dates = gps_obj.dates
+        los_vel = dis_velocity(dates,dis_los) 
+    else:
+        dates, dis_los = gps_obj.read_gps_los_displacement(geometry)
+        gps_obj.get_gps_los_velocity(geometry)
+        los_vel = gps_obj.velocity
+    
+    e_vel = dis_velocity(dates, gps_obj.dis_e)
+    n_vel = dis_velocity(dates, gps_obj.dis_n)
+  
+    site_inve = np.array([[gps_obj.site, gps_obj.site_lat, gps_obj.site_lon, los_vel, n_vel, e_vel]])
+    
+    return site_inve
+
+def dis_velocity(dates, dis):
+    """calculate velocity"""
+    date_list = [dt.strftime(i, '%Y%m%d') for i in dates]
+    if len(date_list) > 2:
+        A = timeseries.get_design_matrix4average_velocity(date_list)
+        vel = np.dot(np.linalg.pinv(A), dis)[0]
+    else:
+        vel = np.nan
+    
+    return vel
+ 
+def search_gpssites(inps):
+    """search gps sites based on given SNWE"""
+    site_names, site_lats, site_lons = gps.search_gps(inps.SNWE)
+    sites_baseinfo = np.transpose(np.vstack((np.vstack((np.array([site_names]),np.array([site_lats]))),np.array([site_lons])))) 
+    
+    return sites_baseinfo
+
+def plot_geotiff(inps, site_infovel=None):
     """read geotiff data"""
 
     shpdir = inps.shpdir[0]
@@ -131,6 +227,18 @@ def plot_geotiff(inps):
     #shp_fault.plot(color='black',ax = ax1,linestyle='dashed',linewidth=1)
     print('\n\n************************************ploting reference point****************************************')
     shp_refpoi.plot(color='black',ax=ax1,marker='s')
+   
+    # plot gps velocity vector field
+    #import pdb
+    #pdb.set_trace()
+    if site_infovel is not None:
+        lon = list(site_infovel[:,2].astype(float))
+        lat = list(site_infovel[:,1].astype(float))
+        x_component = list(site_infovel[:,5].astype(float))
+        y_component = list(site_infovel[:,4].astype(float))
+    
+        ax1.quiver(lon, lat, x_component, y_component, color='deepskyblue')
+
     ax1.tick_params(which='both', direction='in', labelsize=18, bottom=True, top=True, left=True, right=True)
    
     # method 1 for drawing colorbar
@@ -176,7 +284,25 @@ def plot_geotiff(inps):
 ######################################################################################
 def main(iagrs=None):
     inps = cmd_line_parse(iagrs)
-    plot_geotiff(inps)    
+    # whether plot gps velocity vector field
+    if inps.gps_putton:
+        geodata = inps.geometry
+        gpsdir = inps.gpsdir[0]
+        if inps.search:
+            sites_baseinfo = search_gpssites(inps)
+            sites_velocity = np.zeros([sites_baseinfo.shape[0],3])
+            for gpsname, i in zip(sites_baseinfo[:,0],np.arange(sites_baseinfo.shape[0])):
+                site_ve = gps_multisites(str(gpsname),geodata,gpsdir)
+                sites_velocity[i,:] = site_ve
+            site_infovel = np.concatenate((sites_baseinfo,sites_velocity), axis=1)
+            #print(sites_inve)
+        else:
+            gpsname = inps.gps_name
+            site_infovel = gps_single(gpsname, geodata, gpsdir)
+            #print(site_inve)    
+        plot_geotiff(inps,site_infovel)
+    else:
+        plot_geotiff(inps)
 ######################################################################################
 if __name__ == '__main__':
     main()
