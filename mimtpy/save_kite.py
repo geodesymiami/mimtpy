2b8f91a38968845fcde476a4d6ffe17c71ee7d26
ranneylxr <lvxr@radi.ac.cn>
2021-02-21 21:13:20 +0800

    save_kite.py:add subset function

--- mimtpy/save_kite.py
+++ mimtpy/save_kite.py
@@ -19,6 +19,8 @@ EXAMPLE = """example:
   
   save_kite.py S1****.he5 --date 20141016_20171221 -o SenAT115_201410_201712_kite
   
+  save_kite.py S1****.he5 --date 20141016_20171221 --bbox 26 27 64 65 -o SenAT115_201410_201712_kite
+  
   save_kite.py S1****.he5 --date 20141016_20171221 --velocity -o SenAT115_201410_201712_kite
 """
 
@@ -33,6 +35,11 @@ def create_parser():
 
     parser.add_argument('--velocity', action='store_true', default=False, help='whether calculate velocity. \n')
 
+    parser.add_argument('--bbox', dest='SNWE', type=float, nargs=4, metavar=('S', 'N','W','E'),
+                        help='Bounding box of area to be geocoded.\n'+
+                        'Include the uppler left corner of the first pixel' +
+                        'and the lower right corner of the last pixel')
+    
     parser.add_argument('-o','--outfile',dest='outfile',nargs=1,
                         help='outfile name.')
     
@@ -43,6 +50,42 @@ def cmd_line_parse(iargs=None):
     inps = parser.parse_args(args=iargs)  
     
     return inps
+
+def extract_data_based_bbox(inps):
+    """return the row_no,sample_no and rows and samples"""
+    # metadata
+    atr = readfile.read_attribute("".join(inps.input_HDFEOS))
+    ul_lat = float(atr['Y_FIRST'])
+    ul_lon = float(atr['X_FIRST'])
+    lat_step = float(atr["Y_STEP"])
+    lon_step = float(atr["X_STEP"])
+    length = int(atr['LENGTH'])
+    width = int(atr['WIDTH'])
+    lr_lat = ul_lat + lat_step * length
+    lr_lon = ul_lon + lon_step * width
+    # bbox
+    user_lat0 = float(inps.SNWE[1])
+    user_lon0 = float(inps.SNWE[2])
+    user_lat1 = float(inps.SNWE[0])
+    user_lon1 = float(inps.SNWE[3])
+
+    if user_lat0 > ul_lat or user_lat1 < lr_lat or user_lon0 < ul_lon or user_lon1 > lr_lon:
+        print('data range is {} {} {} {}'.format(lr_lat, ul_lat, ul_lon, lr_lon))
+        raise Exception('input bounding box is beyound the data range!\n')
+
+    if user_lat0 < user_lat1:
+        parser.print_usage()
+        raise Exception('input bounding box error! Wrong latitude order!')
+    elif user_lon0 > user_lon1:
+        parser.print_usage()
+        raise Exception('input bounding box error! Wrong longitude order!')
+
+    row = int((user_lat0 - ul_lat) / lat_step + 0.5)
+    sample = int((user_lon0 - ul_lon) / lon_step + 0.5)
+    rows = int((user_lat1 - user_lat0) / lat_step + 0.5) + 1
+    samples = int((user_lon1 - user_lon0) / lon_step + 0.5) + 1
+    
+    return user_lat1, user_lon0, row, sample, rows,samples
 
 def read_HDFEOS(inps):
     """read displacement from HDFEOS"""
@@ -97,6 +140,14 @@ def save_kite(inps):
     # read mintpy data
     disp, disp_atr, incidence, azimuth = read_HDFEOS(inps)
 
+    # subset data based on bbox
+    if inps.SNWE:
+        print('Subset data based on bbox')
+        lat_user, lon_user, row, sample, rows,samples = extract_data_based_bbox(inps)
+        disp = disp[row: row+rows, sample: sample+samples]
+        incidence = incidence[row: row+rows, sample: sample+samples]
+        azimuth = azimuth[row: row+rows, sample: sample+samples]
+    
     # convert to head angle
     print('convert azimuth angle to head angle')
     head = ut.azimuth2heading_angle(azimuth)
@@ -121,6 +172,10 @@ def save_kite(inps):
 
     lat_ll = lat_ul + lat_step * length
     lon_ll = lon_ul
+ 
+    if inps.SNWE:
+        lat_ll = lat_user 
+        lon_ll = lon_user
 
     sc.frame.llLat = lat_ll
     sc.frame.llLon = lon_ll
