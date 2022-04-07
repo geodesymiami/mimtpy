c781f9087e7cef5f2d66a6ad97ee455c335571e5
ranneylxr <lvxr@radi.ac.cn>
2022-04-07 16:41:31 +0800

    mimtpyApp.py:fix words errors

--- mimtpy/mimtpyApp.py
+++ mimtpy/mimtpyApp.py
@@ -14,7 +14,9 @@ from mimtpy.utils import multitrack_utilities as mu
 
 ###############################################################################
 STEP_LIST = [
-     'velcumu',
+     'mask',
+     'coherence',
+     'velocity_displacement',
      'horzvert',
      'concatenation',
      'geodmod',
@@ -46,12 +48,12 @@ def cmd_line_parse(iargs=None):
 
     return inps    
 
-def velcumu(inpsdict):
-    """generate velocity.h5 and velocity.tiff file or generate cumudisp_date1_date2.h5 and cumudisp_date1_date2_tiff."""
-    if inpsdict['mimtpy.velcumu'] == 'yes':
-        Dataset = inpsdict['mimtpy.velcumu.DataSet'] 
-        Dataset = list(tuple([i for i in Dataset.split(',')]))
-        for dataset in Dataset:
+def mask(inpsdict):
+    """generate maskTempCoh.h5 file"""
+    if inpsdict['mimtpy.mask'] == 'yes':
+        datasets = inpsdict['mimtpy.mask.dataset']
+        datasets = list(tuple([i for i in datasets.split(',')]))
+        for dataset in datasets:
             print('\n***********************Start processing {} dataset***********************'.format(dataset))
             # go to dataset_dir
             dataset_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),dataset,'mintpy'))
@@ -61,7 +63,83 @@ def velcumu(inpsdict):
             print('\nGo to project dir:', dataset_dir)
     
             # create velocity or cumulative dir
-            filetype = inpsdict['mimtpy.velcumu.type']
+            filetype = 'maskfile'
+            outdir = os.path.abspath(os.path.join(dataset_dir,filetype))
+            if not os.path.isdir(outdir):
+                os.makedirs(outdir)
+            print('\nthe output dir for {} is {}.\n'.format(filetype,outdir))
+
+            # find the HDFEOS file name
+            HDFEOS_file = mu.find_HDFEOS_fullname(dataset_dir)
+            print('\nThe HDFEOS file is {}'.format(HDFEOS_file))
+
+            scp_args = [HDFEOS_file, 'mask', '--outdir', outdir]
+            scp_args = mu.separate_string_by_space(scp_args)
+            # run HDFEOS_to_geotiff.py
+            print('HDFEOS_to_geotiff.py',scp_args)
+            mimtpy.HDFEOS_to_geotiff.main(scp_args.split())   
+             
+    else:
+        print('\nSkip mask process')
+    return
+
+def coherence(inpsdict):
+    """generate coherence.h5 file"""
+    if inpsdict['mimtpy.coherence'] == 'yes':
+        Coh_type = inpsdict['mimtpy.coherence.type']
+        datasets = inpsdict['mimtpy.coherence.dataset']
+        datasets = list(tuple([i for i in datasets.split(',')]))
+        for dataset in datasets:
+            print('\n***********************Start processing {} dataset***********************'.format(dataset))
+            # go to dataset_dir
+            dataset_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),dataset,'mintpy'))
+            if not os.path.isdir(dataset_dir):
+                raise Exception('Error! No such dir : {}'.format(dataset_dir))
+            os.chdir(dataset_dir)
+            print('\nGo to project dir:', dataset_dir)
+    
+            # create coherence dir
+            filetype = 'coherence'
+            outdir = os.path.abspath(os.path.join(dataset_dir,filetype))
+            if not os.path.isdir(outdir):
+                os.makedirs(outdir)
+            print('\nthe output dir for {} is {}.\n'.format(filetype,outdir))
+
+            # find the HDFEOS file name
+            HDFEOS_file = mu.find_HDFEOS_fullname(dataset_dir)
+            print('\nThe HDFEOS file is {}'.format(HDFEOS_file))
+
+            if inpsdict['mimtpy.coherence.mask'] == 'y':
+                scp_args = [HDFEOS_file, Coh_type, '--mask', '--outdir', outdir]
+                scp_args = mu.separate_string_by_space(scp_args)
+            else:
+                scp_args = [HDFEOS_file, Coh_type, '--outdir', outdir]
+                scp_args = mu.separate_string_by_space(scp_args)
+                
+            # run HDFEOS_to_geotiff.py
+            print('HDFEOS_to_geotiff.py',scp_args)
+            mimtpy.HDFEOS_to_geotiff.main(scp_args.split())  
+             
+    else:
+        print('\nSkip mask process')
+    return
+
+def velocity_displacement(inpsdict):
+    """generate velocity.h5 and velocity.tiff file or generate cumudisp_date1_date2.h5 and cumudisp_date1_date2_tiff."""
+    if inpsdict['mimtpy.velocity_displacement'] == 'yes':
+        datasets = inpsdict['mimtpy.velocity_displacement.dataset'] 
+        datasets = list(tuple([i for i in datasets.split(',')]))
+        for dataset in datasets:
+            print('\n***********************Start processing {} dataset***********************'.format(dataset))
+            # go to dataset_dir
+            dataset_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),dataset,'mintpy'))
+            if not os.path.isdir(dataset_dir):
+                raise Exception('Error! No such dir : {}'.format(dataset_dir))
+            os.chdir(dataset_dir)
+            print('\nGo to project dir:', dataset_dir)
+    
+            # create velocity or cumulative dir
+            filetype = inpsdict['mimtpy.velocity_displacement.type']
             outdir = os.path.abspath(os.path.join(dataset_dir,filetype))
             if not os.path.isdir(outdir):
                 os.makedirs(outdir)
@@ -73,47 +151,47 @@ def velcumu(inpsdict):
             atr = readfile.read_attribute(HDFEOS_file)
     
             # get the startdate and enddate
-            startDate,endDate = mu.find_start_end_date(dataset_dir,inpsdict['mimtpy.velcumu.startDate'],inpsdict['mimtpy.velcumu.endDate'])
+            startDate,endDate = mu.find_start_end_date(dataset_dir,inpsdict['mimtpy.velocity_displacement.startDate'],inpsdict['mimtpy.velocity_displacement.endDate'])
             if startDate == 'None':
                 startDate = atr['START_DATE']
             if endDate == 'None':
                 endDate = atr['END_DATE']
             
             # generate parameter list for HDFEOS_to_geotiff.py
-            if inpsdict['mimtpy.velcumu.SNWE'] == 'None':
-                if inpsdict['mimtpy.velcumu.mask'] == 'y':
+            if inpsdict['mimtpy.velocity_displacement.SNWE'] == 'None':
+                if inpsdict['mimtpy.velocity_displacement.mask'] == 'y':
                    scp_args = [HDFEOS_file, filetype, '--date', startDate+'_'+endDate, '--mask', '--outdir', outdir]
                 else:
                    scp_args = [HDFEOS_file, filetype, '--date', startDate+'_'+endDate, '--outdir', outdir]
             else:
-                SNWE = inpsdict['mimtpy.velcumu.SNWE']
+                SNWE = inpsdict['mimtpy.velocity_displacement.SNWE']
                 SNWE = list(tuple([float(i) for i in SNWE.split(',')]))
-                if inpsdict['mimtpy.velcumu.mask'] == 'y':
+                if inpsdict['mimtpy.velocity_displacement.mask'] == 'y':
                    scp_args = [HDFEOS_file, filetype, '--date', startDate+'_'+endDate, '--mask', '--bbox', SNWE, '--outdir', outdir]
                 else:
                    scp_args = [HDFEOS_file, filetype, '--date', startDate+'_'+endDate, '--bbox', SNWE, '--outdir', outdir]
 
-            scp_args = mu.seperate_str_byspace(scp_args)
+            scp_args = mu.separate_string_by_space(scp_args)
             # run HDFEOS_to_geotiff.py
             print('HDFEOS_to_geotiff.py',scp_args)
             mimtpy.HDFEOS_to_geotiff.main(scp_args.split())   
     else:
-        print('\nSkip velcumu process')
+        print('\nSkip velocity_displacement process')
     
     return
 
 def horzvert(inpsdict):
     """generate horzontal and vertical files."""
     if inpsdict['mimtpy.horzvert'] == 'yes':
-        Dataset = inpsdict['mimtpy.horzvert.DataSet']
-        Dataset = list(tuple([i for i in Dataset.split(',')]))
+        datasets = inpsdict['mimtpy.horzvert.dataset']
+        datasets = list(tuple([i for i in datasets.split(',')]))
      
-        asc_data = Dataset[0]
+        asc_data = datasets[0]
         asc_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),asc_data,'mintpy'))
         if not os.path.isdir(asc_dir):
             raise Exception('Error! No such dir : {}'.format(asc_dir))
                
-        des_data = Dataset[1]
+        des_data = datasets[1]
         des_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),des_data,'mintpy'))
         if not os.path.isdir(des_dir):
             raise Exception('Error! No such dir : {}'.format(des_dir))
@@ -172,7 +250,7 @@ def horzvert(inpsdict):
             scp_args2 = [file_asc_data, file_des_data, '--bbox', SNWE, '--outname', outname, '--outdir', outdir]
         elif refpoi_lalo == 'None' and azimuth == 'None' and outname == 'None':
             scp_args2 = [file_asc_data, file_des_data, '--bbox', SNWE, '--outdir', outdir]
-        scp_args2 = mu.seperate_str_byspace(scp_args2)
+        scp_args2 = mu.separate_string_by_space(scp_args2)
         print('\ngenerate_horzvert.py',scp_args2)
         mimtpy.generate_horzvert.main(scp_args2.split())
     else:
@@ -183,18 +261,18 @@ def horzvert(inpsdict):
 def concatenation(inpsdict):
     """concatenation adjencant tracks."""
     if inpsdict['mimtpy.concatenation'] == 'yes':
-        Dataset = inpsdict['mimtpy.concatenation.DataSet']
-        Dataset = list(tuple([i for i in Dataset.split(',')]))
+        datasets = inpsdict['mimtpy.concatenation.dataset']
+        datasets = list(tuple([i for i in datasets.split(',')]))
         
-        project_master = Dataset[0]
-        promaster_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),project_master,'mintpy'))
-        if not os.path.isdir(promaster_dir):
-            raise Exception('Error! No such dir : {}'.format(promaster_dir))
+        project_dominant = datasets[0]
+        prodominant_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),project_dominant,'mintpy'))
+        if not os.path.isdir(prodominant_dir):
+            raise Exception('Error! No such dir : {}'.format(prodominant_dir))
         
-        project_slave = Dataset[1]
-        proslave_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),project_slave,'mintpy'))
-        if not os.path.isdir(proslave_dir):
-            raise Exception('Error! No such dir : {}'.format(proslave_dir))
+        project_affiliate = datasets[1]
+        proaffiliate_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),project_affiliate,'mintpy'))
+        if not os.path.isdir(proaffiliate_dir):
+            raise Exception('Error! No such dir : {}'.format(proaffiliate_dir))
         
         # create workdir
         workdir = inpsdict['mimtpy.concatenation.outdir']
@@ -206,10 +284,10 @@ def concatenation(inpsdict):
         # file type
         files = inpsdict['mimtpy.concatenation.dataname']
         files = list(tuple([i for i in files.split(',')]))
-        file_master = files[0]
-        file_slave = files[1]
+        file_dominant = files[0]
+        file_affiliate = files[1]
        
-        file_type = file_master.split('_')[0]
+        file_type = file_dominant.split('_')[0]
         # out put dir
         outdir = os.path.abspath(os.path.join(workdir,file_type)) + '/'
         if not os.path.isdir(outdir):
@@ -221,37 +299,37 @@ def concatenation(inpsdict):
         plotpair_opt = inpsdict['mimtpy.concatenation.plotpair']
 
         # file_dir
-        fmaster_dir = os.path.abspath(os.path.join(promaster_dir,file_type)) + '/'
-        if not os.path.isdir(fmaster_dir):
-            raise Exception('Error! No such dir : {}'.format(fmaster_dir))
+        fdominant_dir = os.path.abspath(os.path.join(prodominant_dir,file_type)) + '/'
+        if not os.path.isdir(fdominant_dir):
+            raise Exception('Error! No such dir : {}'.format(fdominant_dir))
         
-        fslave_dir = os.path.abspath(os.path.join(proslave_dir,file_type)) + '/'
-        if not os.path.isdir(fslave_dir):
-            raise Exception('Error! No such dir : {}'.format(fslave_dir))
+        faffiliate_dir = os.path.abspath(os.path.join(proaffiliate_dir,file_type)) + '/'
+        if not os.path.isdir(faffiliate_dir):
+            raise Exception('Error! No such dir : {}'.format(faffiliate_dir))
         
-        # master and slave data name
-        Dmaster = fmaster_dir + file_master + '.h5'
-        Dslave = fslave_dir + file_slave + '.h5'
+        # dominant and affiliate data name
+        Ddominant = fdominant_dir + file_dominant + '.h5'
+        Daffiliate = faffiliate_dir + file_affiliate + '.h5'
         
         # mosaic file name
         if inpsdict['mimtpy.concatenation.outname'] != 'None':
             outname = inpsdict['mimtpy.concatenation.outname']
         else:
-            outname = file_master + '_mosaic'
+            outname = file_dominant + '_mosaic'
 
         if rewrite_opt == 'y':
             if plotpair_opt == 'y':
                 azi_angle = float(inpsdict['mimtpy.concatenation.azimuth'])
-                scp_args = [Dmaster, Dslave, '--rewrite_slave', '--output', outname, '--plotpair', '--azi_angle', azi_angle, '--outdir', outdir]
+                scp_args = [Ddominant, Daffiliate, '--rewrite_affiliate', '--output', outname, '--plotpair', '--azi_angle', azi_angle, '--outdir', outdir]
             elif plotpair_opt == 'n':
-                scp_args = [Dmaster, Dslave, '--rewrite_slave', '--output', outname, '--outdir', outdir]
+                scp_args = [Ddominant, Daffiliate, '--rewrite_affiliate', '--output', outname, '--outdir', outdir]
         elif rewrite_opt == 'n':
             if plotpair_opt == 'y':
                 azi_angle = float(inpsdict['mimtpy.concatenation.azimuth'])
-                scp_args = [Dmaster, Dslave, '--output', outname, '--plotpair', '--azi_angle', azi_angle, '--outdir', outdir]
+                scp_args = [Ddominant, Daffiliate, '--output', outname, '--plotpair', '--azi_angle', azi_angle, '--outdir', outdir]
             elif plotpair_opt == 'n':
-                scp_args = [Dmaster, Dslave, '--output', outname, '--outdir', outdir]
-        scp_args = mu.seperate_str_byspace(scp_args)
+                scp_args = [Ddominant, Daffiliate, '--output', outname, '--outdir', outdir]
+        scp_args = mu.separate_string_by_space(scp_args)
         
         # run track_offset.py
         print('track_offset.py',scp_args)
@@ -261,7 +339,7 @@ def concatenation(inpsdict):
         # mosaic data
         Dmosaic = outdir + outname + '.h5'
         scp_args2 = [Dmosaic, '--outdir', outdir, '--output', outname + '.tiff'] 
-        scp_args2 = mu.seperate_str_byspace(scp_args2)
+        scp_args2 = mu.separate_string_by_space(scp_args2)
         
         print('\nH5UNW_to_geotiff.py',scp_args2)
         mimtpy.H5UNW_to_geotiff.main(scp_args2.split())
@@ -275,9 +353,9 @@ def plot(inpsdict):
         print('Start ploting data')
         plot_type = inpsdict['mimtpy.plot.type']
         if plot_type == 'velocity' or plot_type == 'displacement':
-            Dataset = inpsdict['mimtpy.velcumu.DataSet'] 
-            Dataset = list(tuple([i for i in Dataset.split(',')]))
-            for dataset in Dataset:
+            datasets = inpsdict['mimtpy.velocity_displacement.dataset'] 
+            datasets = list(tuple([i for i in datasets.split(',')]))
+            for dataset in datasets:
                 # go to dataset_dir
                 dataset_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),dataset,'mintpy'))
                 if not os.path.isdir(dataset_dir):
@@ -316,7 +394,7 @@ def plot(inpsdict):
                         else:
                             scp_args = [filename, '--outfile', outfile, '--outdir', plot_dir]
 
-                        scp_args = mu.seperate_str_byspace(scp_args)
+                        scp_args = mu.separate_string_by_space(scp_args)
                         # run plot_geotiff.py
                         print('\n*********************ploting %s file********************' % Vfile)
                         print('plot_geotiff.py',scp_args)
@@ -329,9 +407,9 @@ def plot(inpsdict):
 def geodmod(inpsdict):
     """prepare data for geodmod software"""
     if inpsdict['mimtpy.geodmod'] == 'yes':
-        Dataset = inpsdict['mimtpy.geodmod.DataSet'] 
-        Dataset = list(tuple([i for i in Dataset.split(',')]))
-        for dataset in Dataset:
+        datasets = inpsdict['mimtpy.geodmod.dataset'] 
+        datasets = list(tuple([i for i in datasets.split(',')]))
+        for dataset in datasets:
             print('\n***********************Start processing {} dataset***********************'.format(dataset))
             # go to dataset_dir
             dataset_dir = os.path.abspath(os.path.join(os.getenv('SCRATCHDIR'),dataset,'mintpy'))
@@ -370,7 +448,7 @@ def geodmod(inpsdict):
                 SNWE = list(tuple([float(i) for i in SNWE.split(',')]))
                 scp_args = ['../' + HDFEOS_file, '-s', startDate, '-e', endDate, '--bbox', SNWE]
 
-            scp_args = mu.seperate_str_byspace(scp_args)
+            scp_args = mu.separate_string_by_space(scp_args)
             # run save_geodmod.py
             print('save_geodmod.py',scp_args)
             mimtpy.save_geodmod.main(scp_args.split())   
@@ -381,8 +459,8 @@ def run(inpsdict, steps=STEP_LIST):
     """run the chosen steps."""
     for sname in steps:
         print('\n**************************step - {}***********************'.format(sname))
-        if sname == 'velcumu':
-            velcumu(inpsdict)
+        if sname == 'velocity_displacement':
+            velocity_displacement(inpsdict)
         elif sname == 'horzvert':
             horzvert(inpsdict)
         elif sname == 'concatenation':
@@ -392,6 +470,10 @@ def run(inpsdict, steps=STEP_LIST):
             plot(inpsdict)  
         elif sname == 'geodmod':
             geodmod(inpsdict)
+        elif sname == 'mask':
+            mask(inpsdict)
+        elif sname == 'coherence':
+            coherence(inpsdict)
     return        
 
 ######################################################################################
